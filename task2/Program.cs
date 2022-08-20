a8453570a0ce4062d189a14d3cef3686cd92f5ce
Natalia Frolova <n.smirnova76@mail.ru>
2022-08-20 14:58:21 +0300

    Решила домашнее задание

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int numberA = new Random().Next(1, 1000);
+Console.WriteLine("первое число: "+numberA);
+int numberB = new Random().Next(1, 1000); 
+Console.WriteLine("второе число: "+numberB);
+
+int max = numberA;
+int min = numberB;
+
+if (numberB > max)
+{
+    max = numberB;
+    min = numberA;
+}
+else
+{
+    max = numberA;
+    min = numberB;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.Write(min);
---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int numberA = new Random().Next(1, 1000);
+Console.WriteLine("первое число: " + numberA);
+int numberB = new Random().Next(1, 1000);
+Console.WriteLine("второе число: " + numberB);
+int numberC = new Random().Next(1, 1000);
+Console.WriteLine("второе число: " + numberC);
+
+int max = numberA;
+
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
